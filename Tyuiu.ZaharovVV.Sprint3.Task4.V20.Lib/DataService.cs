3e81b22bc0d4ef19b870c95ddb8276f3712920f6
ZaharovVladimir <chibisntbshow@gmail.com>
2024-10-30 07:08:13 +0500

    task4

--- Tyuiu.ZaharovVV.Sprint3.Task4.V20.Lib/DataService.cs
+++ Tyuiu.ZaharovVV.Sprint3.Task4.V20.Lib/DataService.cs
@@ -1,7 +1,24 @@
-﻿namespace Tyuiu.ZaharovVV.Sprint3.Task4.V20.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
 
+namespace Tyuiu.ZaharovVV.Sprint3.Task4.V20.Lib
+{
+    public class DataService : ISprint3Task4V20
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 1;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    res = res * (x / (Math.Cos(x) - x) + 2.5);
+                }
+            }
+            return Math.Round(res, 3);
+        }
     }
 }
--- Tyuiu.ZaharovVV.Sprint3.Task4.V20.Test/DataServiceTest.cs
+++ Tyuiu.ZaharovVV.Sprint3.Task4.V20.Test/DataServiceTest.cs
@@ -1,11 +1,24 @@
+using Tyuiu.ZaharovVV.Sprint3.Task4.V20.Lib;
+
 namespace Tyuiu.ZaharovVV.Sprint3.Task4.V20.Test
 {
     [TestClass]
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidCalculate()
         {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int endValue = 5;
+
+            double res = ds.Calculate(startValue, endValue);
+
+            double wait = 4.676;
+
+            Assert.AreEqual(res, wait);
+
         }
     }
 }
